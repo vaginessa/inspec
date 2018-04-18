916d693fd6bfbb466be0d277e7d2a5017b3e7283
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-04-18 16:14:34 -0400

    Simple profile that triggers the shadow resource chaining issue
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

---
+++ test/regression/shadow-chaining-with-check/profile-001/controls/example.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+# copyright: 2018, The Authors
+
+title 'sample section'
+
+plat = platform
+
+describe "My platform: #{plat}" do
+  it { should include 'plat' }
+end
+
+describe shadow do 
+  its('count') { should be > 0 }
+end
+
+control 'Unchained shadow in a control' do
+  describe shadow do 
+    its('count') { should be > 0 }
+  end    
+end
+
+control 'Chained shadow in a control' do
+  # kaboom
+  users = shadow.filter(password:/[^!|*]/).params.map { |x| x['user'] }
+  describe shadow do 
+    its('count') { should be > 0 }
+  end    
+end
