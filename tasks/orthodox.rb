d6fb36b6dbac87299181dd6592d6b8a4069cb1bb
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-06-22 17:57:52 -0400

    A simple mechanism for complaining about docs
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- tasks/orthodox.rb
+++ tasks/orthodox.rb
@@ -2,6 +2,7 @@
 
 require 'commonmarker'
 require 'octokit'
+require 'yaml'
 
 module Orthodoxy
   class Releases
@@ -51,6 +52,68 @@ module Orthodoxy
       matches
     end
   end
+
+  Gripe = Struct.new(:severity, :location, :message, :peeve_name)
+
+  class Peeve
+    attr_reader :name
+    attr_reader :checker
+    attr_reader :message_maker
+    attr_accessor :gripes
+
+    def initialize(name, checker, message_maker)
+      @name = name
+      @checker = checker
+      @message_maker = message_maker
+      @gripes = []
+    end
+
+    def check(node)
+      checker.call(node)
+      gripes
+    end
+
+    def message(node)
+      message_aker.call(node)
+    end
+
+    def add_gripe(node, severity=:fail, message=nil)
+      loc = unpack_location(node)
+      message ||= message(node)
+      @gripes << Gripe.new(severity, loc, message, name)
+    end
+
+    private
+    def unpack_location(node)
+      pos = node.source_pos
+      "#{pos.start_line}:#{pos.start_column}-#{pos.end_line}:#{pos.end_column}"
+    end
+  end
+
+  class WhingeOMatic
+    attr_reader :peeves
+    attr_reader :gripes
+    def initialize
+      @peeves = []
+      @gripes = []
+    end
+
+    def load_peeves
+      peeves << Peeve.new(
+        'UnescapedUnderScoreInHeading',
+        ->(node) do
+          return [] unless node.type == :header
+          if node.each do |header_text_node|
+            header_text_node.type == :text and
+            header_text_node.string_content =~ /[^\\]_/
+          end then
+            add_gripe(header_text_node,:warn)
+          end
+        end,
+        ->(node) { 'okey-dokey' },
+      )
+    end
+  end
 end
 
 namespace :orthodox do
@@ -97,10 +160,26 @@ namespace :orthodox do
 
   desc 'Look for things to complain about in each resource doc file'
   task :whinge do
-    # List Files
-    # Open and parse as markdown
-    # Apply each rule to the markdown AST
-    # summarize
+    # by peeve, by doc, gripes, by field
+    results = {}
+
+    whinger = Orthodoxy::WhingeOMatic.new
+    whinger.load_peeves # TODO filter here
+    whinger.peeves.each do |peeve|
+      peeve_results = {}
+      Dir.glob('docs/resources/*.md.erb').sort.each do |md_file|
+        resource_doc = Orthodoxy::ResourceDoc.new(md_file)
+        doc_results = []
+        resource_doc.doc.walk do |node|
+          peeve.check(node).each do |gripe|
+            doc_results << gripe.to_h
+          end
+        end
+        peeve_results[resource_doc.name] = doc_results unless doc_results.empty?
+      end
+      results[peeve.name] = peeve_results unless peeve_results.empty?
+    end
+    puts({ 'whinge' => results }.to_yaml)
   end
 
   desc 'Search markdown files and emit YAML'
