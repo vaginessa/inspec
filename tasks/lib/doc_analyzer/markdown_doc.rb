5698f8cf251b51aca6babb9995848d1fb4533091
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-07-06 17:40:53 -0400

    Add section finders to find upper and lower sections
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- tasks/lib/doc_analyzer/markdown_doc.rb
+++ tasks/lib/doc_analyzer/markdown_doc.rb
@@ -26,5 +26,35 @@ module DocAnalyzer
       end
       matches
     end
+
+    # If 'node' is a L3 header, this searches back until it finds
+    # the previous L2 header (if any).  To humans, this looks like
+    # the L3 is "in" the L2, but it isn't markdown-wise.
+    def previous_higher_level_header(node)
+      higher_header = nil
+      orig_level = node.header_level
+      cursor = node
+      while cursor = cursor.previous do
+        next unless cursor.type == :header
+        higher_header = cursor if cursor.header_level < orig_level
+        break if higher_header
+      end
+      higher_header
+    end
+
+    # If node is a L2 header, this lists all L3 headers until we
+    # hit another L2 header, or hit the end of the doc.
+    def following_lower_level_headers(node)
+      lower_headers = []
+      orig_level = node.header_level
+      cursor = node
+      while cursor = cursor.next do
+        next unless cursor.type == :header
+        break if cursor.header_level < orig_level
+        lower_headers << cursor if cursor.header_level == orig_level + 1
+      end
+      lower_headers
+    end
+
   end
 end
