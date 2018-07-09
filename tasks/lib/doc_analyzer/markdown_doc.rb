9030aa14bde23d126e4242fc61c10e838c5ccfb4
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-07-09 16:30:17 -0400

    Fragment injection and doc save
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- tasks/lib/doc_analyzer/markdown_doc.rb
+++ tasks/lib/doc_analyzer/markdown_doc.rb
@@ -5,11 +5,13 @@ require 'commonmarker'
 module DocAnalyzer
   class MarkdownDoc
 
-    attr_reader :doc, :name
+    attr_reader :doc, :name, :orig_content
 
     def initialize(path)
       @name = path.split('/').last.split('.').first
-      @doc = CommonMarker.render_doc(File.read(path))
+      @orig_content = File.read(path)
+      @doc = CommonMarker.render_doc(orig_content)
+      scrub_metadata_garbage
     end
 
     def find_sections(name, level = :any)
@@ -25,6 +27,17 @@ module DocAnalyzer
         end
       end
       matches
+    end
+
+    def find_first_section(*header_names)
+      first_header = nil
+      header_names.each do |header_name|
+        header = find_sections(header_name).first
+        next unless header
+        first_header ||= header
+        first_header = header if header.sourcepos[:start_line] < first_header.sourcepos[:start_line]
+      end
+      first_header
     end
 
     # If 'node' is a L3 header, this searches back until it finds
@@ -56,5 +69,48 @@ module DocAnalyzer
       lower_headers
     end
 
+    def inject_fragment_before(fragment, node)
+      doc_fragment = CommonMarker.render_doc(fragment)
+      source_cursor = doc_fragment.last_child
+      target_cursor = node
+      while source_cursor do
+        # Must not perform the insertion on the cursor - it will get its pointers
+        # updated to point into the target doc.  So, grab a copy of the cursor,
+        # then advance it, then do the insert.
+        inserter = source_cursor
+        source_cursor = source_cursor.previous
+        target_cursor.insert_before(inserter)
+        target_cursor = target_cursor.previous
+      end
+    end
+
+    # We need a special write, because we insert non-md garbage metadata at the top of the file.
+    def write(path)
+      # ---
+      # title: About the google_container_node_pool Resource
+      # platform: gcp
+      # ---
+      match = orig_content.match(/(---\ntitle:.+\nplatform:.+\n---\n)/m)
+      unless match
+        warn "WARN: refusing to write #{path}, could not find metadata block"
+        return
+      end
+      metadata = match[1]
+      rendered_markdown = doc.to_commonmark
+      File.write(path, metadata + "\n" + rendered_markdown)
+    end
+
+    private
+    def scrub_metadata_garbage
+      # The metadata block typically gets turned into a horizontal rule and a level two header
+      hr = doc.first_child
+      if hr.type == :hrule
+        hr.delete
+      end
+
+      l2 = find_sections('^title:', 2).first
+      l2.delete if l2
+
+    end
   end
 end
